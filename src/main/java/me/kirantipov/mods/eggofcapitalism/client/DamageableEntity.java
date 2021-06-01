9da1b2ec5fd942e9ca9ecbcdf208801648c642c9
Kir-Antipov <kp.antipov@gmail.com>
2021-06-01 21:02:40 +0300

    Documented the code

--- src/main/java/me/kirantipov/mods/eggofcapitalism/client/DamageableEntity.java
+++ src/main/java/me/kirantipov/mods/eggofcapitalism/client/DamageableEntity.java
@@ -2,6 +2,16 @@ package me.kirantipov.mods.eggofcapitalism.client;
 
 import net.minecraft.entity.damage.DamageSource;
 
+/**
+ * Represents an entity that can take damage.
+ */
 public interface DamageableEntity {
+
+    /**
+     * Returns the last damage source, if present;
+     * otherwise null.
+     *
+     * @return The last damage source, if any; otherwise null.
+     */
     DamageSource getLastDamageSource();
 }
--- src/main/java/me/kirantipov/mods/eggofcapitalism/mixin/MixinLivingEntity.java
+++ src/main/java/me/kirantipov/mods/eggofcapitalism/mixin/MixinLivingEntity.java
@@ -6,11 +6,17 @@ import net.minecraft.entity.damage.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
+/**
+ * Mixin that implements DamageableEntity interface for the LivingEntity class.
+ */
 @Mixin(LivingEntity.class)
 public class MixinLivingEntity implements DamageableEntity {
     @Shadow
     private DamageSource lastDamageSource;
 
+    /**
+     * {@inheritDoc}
+     */
     public DamageSource getLastDamageSource() {
         return lastDamageSource;
     }
